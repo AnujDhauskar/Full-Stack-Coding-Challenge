7e0b4ea3aa5baec1959eca1e08a602cce7fd9805
Anuj Dhauskar <anujdhauskar64@gmail.com>
2026-07-09 13:05:12 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.json(
+        {message:"serevr is running"}
+    )
+})
+
+
+
+export default app;
