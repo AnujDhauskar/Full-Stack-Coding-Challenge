82b7ee0073878c5535248bb8f00f495c1de872a5
Anuj Dhauskar <anujdhauskar64@gmail.com>
2026-07-11 11:39:17 +0530

    feat: initialize Sequelize database connection with MySQL configuration

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -9,9 +9,17 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
     pool: {
       max: 10,
       min: 0,
